e4a9c16565c9b424c20b9c4022e6a202575cf27b
Tony Qian <tqian@stellar-intel.princeton.edu>
2021-08-10 19:15:47 -0400

    upgrading run-fieldline.py

--- scripts/trace/run-fieldline.py
+++ scripts/trace/run-fieldline.py
@@ -1,23 +1,73 @@
 import numpy as np
+from FICUS import MgridIO 
 import sys
 import os
 
-# usage: python run-fieldlines.py <list-mgrid-files>
-# updated 19 Jan 2021
+'''
+    This program reads an netCDF
+    writes the appropriate XFIELDLINES input file
+    and runs the job on slurm
 
-files = sys.argv[1:]
-for f in files:
+    Updated 5 July 2021
+'''
 
-        mgrid = f
-        vmec = f[12:] + '-fl'
-        print(' launching: %s' % mgrid)
+### Read
+try:
+    f_mgrid = sys.argv[1]
 
-        # run on cluster
-        os.system('srun -N 2 -t 2:00:00 --mem=8GB xfieldlines -vmec %s -mgrid %s -vac -auto &'%(vmec, mgrid))  # for eddy
-        #os.system('srun -n 4 -t 2:00:00 --mem=8GB xfieldlines -vmec %s -mgrid %s -vac -auto &'%(vmec, mgrid)) # for portal
+except:
+    print(' usage: python run-fieldlines.py mgrid.nc')
+    sys.exit()
 
-        # run local
-        #cmd = 'xfieldlines -vmec {} -mgrid {} -vac -auto >| log.{} &'.format(vmec, mgrid, vmec)
-        #print(cmd)
-        #os.system(cmd)
+mgrid = MgridIO.Read_MGRID()
+mgrid.read_netCDF(f_mgrid)
+
+
+### Edit
+f_sample = 'input.101-test'
+with open(f_sample) as f:
+    sample_input = f.readlines()
+
+# find line where FIELDLINES input starts
+start = 0
+for line in sample_input:
+    if (line.find( '&FIELDLINES_INPUT' ) > -1):
+        break
+    start += 1
+total = len(sample_input)
+
+knobs = ['NR', 'NPHI', 'NZ', 'RMIN', 'RMAX', 'ZMIN', 'ZMAX']
+value = [mgrid.nr, mgrid.nphi, mgrid.nz, mgrid.rmin, mgrid.rmax, mgrid.zmin, mgrid.zmax]
+N_edits = len(knobs)
+
+def write_input(f_new):
+
+
+    with open(f_new, 'w') as f:
+
+        for i in np.arange(total):
+
+            line = sample_input[i]
+            if (i < start):
+                f.write(line) 
+                continue
+
+            for j in np.arange(N_edits):
+                knob = knobs[j]
+                if (line.find( knob ) > -1):
+                    line = ' {:15} =   {},\n'.format(knob, value[j])
+                    #print('  %s'%line)
+            f.write(line)
+
+        print('  writing {:40}: {}'.format(f_new, value ) )
+
+tag = f_mgrid[:-3]
+fout = 'input.' + tag
+write_input(fout)
+
+
+### Run
+f_log = 'log.' + tag
+os.system('srun -t 1:00:00 xfieldlines -vmec {} -mgrid {} -vac -auto > {} &'.format(tag,f_mgrid,f_log) )
+print(' saving log to:', f_log)
 
