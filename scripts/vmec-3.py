3a6bc5a34ae36af64dc360fc9bf61e4cbfe8854d
Tony Qian <tqian@stellar-intel.princeton.edu>
2021-10-21 23:12:09 -0400

    adding stellopt plot scripts

---
+++ scripts/vmec-3.py
@@ -0,0 +1,50 @@
+from scripts import StelloptReader as sr
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+'''
+  usage: python plot_stellopt.py <file_tag> <(opt)boundary.plasma>
+
+  Looking for
+     wout_fname.nc
+     boozmn_fname.nc
+     neo_out.fname
+
+  Give optional 2nd file, if you want to draw a target boundary on the vmec equilibrium.
+  This takes FOCUS .plasma format
+
+  Updated September 28, 2021
+'''
+fname = sys.argv[1]
+
+f_vmec = 'wout_'   + fname +'.nc'
+
+print('reading vmec file: ', f_vmec)
+vd = sr.readVMEC(f_vmec)
+
+N = 50
+nfp = vd.nfp
+s = -11
+plt.figure()
+for phi in [0, 1/nfp/2, 1/nfp]:
+    R,Z = vd.get_surface(N,phi=phi*np.pi,s=s)
+    if phi > 0:
+        tag = r'$\pi$/{}'.format(int(1/phi))
+    else:
+        tag = 0
+    plt.plot(R,Z,label=tag)
+
+plt.title(fname)
+
+#sr.plot_circle()
+#vd.plot_vmec_3( phi=[0,np.pi/4, np.pi/2] )  # default setting for NFP=2
+plt.legend()
+plt.axis('square')
+
+plt.xlabel('R [m]')
+plt.ylabel('Z [m]')
+
+plt.draw()
+plt.show()
+
